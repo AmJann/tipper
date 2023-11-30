dc85dea1488ad8253fee308aa931ee31ddc1f1af
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-11-30 15:47:49 -0500

    comments can now be submitted successfully

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Header from './Header'
 
-function CommentForm() {
+function CommentForm(id) {
+    const [comment,setComment] = useState('')
+    const user = 1;
+    const resetForm = () => {
+        setComment('');
+      };
+
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault();
+        
+        console.log(JSON.stringify({
+            comment:comment,
+            user: user,
+            post_comment: id.id
+      }))
+        const url = process.env.REACT_APP_API_URL;
+        const fullUrl = `${url}/comment-list/`;
+        
+        try {
+            const response = await fetch(fullUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    comment:comment,
+                    user: user,
+                    post_comment: id.id
+                }),
+            });
+      
+            if (response.ok) {
+                console.log('Comment submitted successfully!');
+                resetForm()
+            } 
+            else {
+                console.error('Failed to submit comment:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error submitting comment:', error.message);
+        }
+      };
   return (
     <div>
-        <form>
+        <form onSubmit={handleCommentSubmit}>
             <label>
-                <textarea type='text'placeholder='comment'cols="41" rows="5"></textarea>
+                <textarea type='text'placeholder='comment'cols="41" rows="5" onChange={(e) => setComment(e.target.value)}></textarea>
             </label>
+            <button type='submit'>Submit Comment</button>
         </form>
     </div>
   )
